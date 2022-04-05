85270caaf2c2581f110e4874f0fd7cc900abd2ba
wm1293 <wm1293@gmail.com>
2022-04-05 10:49:04 -0300

    actualizacion martes 5

--- main.cpp
+++ main.cpp
@@ -10,7 +10,14 @@
 #include "datatypes/headers/DtPartida.h"
 #include "datatypes/headers/DtFechaHora.h"
 
-//DtJugador** obtenerJugadores(int &cantJugadores);
+int cantJugadores = 0;
+const int MAX_JUGADORES = 10;
+
+Jugador **jugadores = new Jugador *[MAX_JUGADORES];
+bool obtenerJugadorPorNickname(string nickname);
+void agregarJugador(string nickname, int edad, string pass);
+void mostrarJugadores(int &cantJugadores);
+DtJugador** obtenerJugadores(int &cantJugadores);
 
 
 
@@ -18,9 +25,13 @@ using namespace std;
 
 int main(){
     
-    int comando; 
+    int comando = 0;
 
-    cout << "Elija una opción.\n" << endl;
+    
+
+    while (comando != 7 || comando == 0)
+    {
+    std::cout << "Elija una opción.\n" << endl;
     std::cout << "1) Agregar Jugador.\n" << endl;
     std::cout << "2) Agregar Videojuego.\n" << endl;
     std::cout << "3) Obtener Jugadores.\n" << endl;
@@ -31,55 +42,175 @@ int main(){
 
     cin >> comando;
 
-    while (comando != 7)
-    {
         if(comando == 1){
-           //Jugador *j;
-           string pass,nick;
-           int edad;
-            cout << "Vamos a crar un nuevo jugador:\n" << endl;
-
-            cout << "Nickname: \n";
-            cin >> nick;
-
-            cout << "Edad \n";
-            cin >> edad;
-
-            cout << "Contrasenia: \n";
-            cin >> pass;
-
            
-           break;        
+           
+           string pass,nickname;
+           int edad;
+         string res;
+            std::cout << "Vamos a crar un nuevo jugador:\n" << endl;
+
+            
+           
+            
+            do 
+            {
+               
+
+                std::cout << "Nickname: \n";
+                cin >> nickname;
+
+                std::cout << "Edad \n";
+                cin >> edad;
+
+                std::cout << "Contrasenia: \n";
+                cin >> pass;
+
+                agregarJugador(nickname,edad,pass);
+
+                 std::cout << "Desea agregar otro jugador ? SI/NO" << endl;
+                 cin >> res;
+            }
+            while ((res == "SI" || res == "si"));
+            
+            
+            
+            comando = 0;  //salgo al menu principal    
+            
+               
 
 
         }
 
         if(comando == 2){
-            cout << "Agregar Videojuegos a desarrollar...";
+            std::cout << "Agregar Videojuegos a desarrollar...";
+              
+
         }
 
-        if(comando == 3){
-            cout << "Obtener Jugadores a desarrollar...";
+        if(comando == 3){   
+
+            int cantJugadores;
+            
+            cout << "Ingresar la cantidad de jugadores a mostrar. Maximo 10" << endl;
+            cin >> cantJugadores;
+
+            DtJugador **dtJugadores = new DtJugador*[cantJugadores];
+            dtJugadores = obtenerJugadores(cantJugadores);
+            //obtenerJugadores(cantJugadores);
+            //mostrarJugadores(cantJugadores);        
+            for (int i = 0 ; i < cantJugadores ; i++){
+                cout << "Nickname: " << dtJugadores[i]->getNickname() << endl;
+            }
+            
         }
 
         if(comando == 4){
-            cout << "Obtener Videojuego a desarrollar...";            
+            std::cout << "Obtener Videojuego a desarrollar...";            
         }
 
         if(comando == 5){
-            cout << "Obtener Partidas. a desarrollar...";  
+            std::cout << "Obtener Partidas. a desarrollar...";  
         }
 
         if (comando == 6){
-            cout << "Iniciar Partida a desarrollar";
+            std::cout << "Iniciar Partida a desarrollar";
         }
 
         if (comando == 7){
-            cout << "Saliste....";
+            std::cout << "Saliste....";
         }
+         system("pause");
     }
+
+   
 
    
     
     return 0;
+
 }
+
+DtJugador** obtenerJugadores(int &cantJugadores){  
+   
+   std::cout << "Entramos en OBTENER Jugadores" << endl;
+   /*
+    for (int i = 0; i < cantJugadores ; i++){
+        std::cout << "Nickname: " << jugadores[i]->getNickname() << " Edad: " << jugadores[i]->getEdad() << endl;
+    }
+
+   */
+
+ // creamos el arreglo de Dt Jugadores que vamos a retornar
+    //DtJugador** jugadores = new DtJugador *[cantJugadores];
+    DtJugador **Dtjugadores = new DtJugador*[cantJugadores];
+    
+    //un for con la cantidad de jugadores a devolver
+    
+    //cargo los valores en el dt
+    for (int i = 0; i < cantJugadores ; i++){
+        Dtjugadores[i]->setNickname(jugadores[i]->getNickname());
+        Dtjugadores[i]->setEdad(jugadores[i]->getEdad());
+    }
+        //muestro los valores
+    /*std::cout << "Mostramos Jugadores" << endl;
+    for (int i = 0; i < cantJugadores ; i++){
+        std::cout << "Nickname: " << Dtjugadores[i]->getNickname() << " Edad: " << Dtjugadores[i]->getEdad() << endl;
+    }*/
+
+
+    return Dtjugadores; 
+} 
+
+
+
+
+void agregarJugador(string nickname, int edad, string pass){
+
+    
+    
+    bool existeJugador = obtenerJugadorPorNickname(nickname);
+    
+
+    if(existeJugador){
+                
+                
+        throw std::invalid_argument("El Jugador ya existe.");       
+                
+    }  
+     
+    if(existeJugador == false)
+    {                      
+        
+         Jugador *jugador = new Jugador(nickname,edad,pass);
+         jugadores[cantJugadores] = jugador;
+         cantJugadores++;
+         
+    }      
+    
+
+}
+
+void mostrarJugadores(int &cantJugadores){
+    
+    
+    
+    for (int i = 0; i < cantJugadores ; i++){
+        std::cout << "Nickname: " << jugadores[i]->getNickname() << " Edad: " << jugadores[i]->getEdad() << endl;
+    }
+
+   
+}
+
+bool obtenerJugadorPorNickname(string nickname){
+    
+    for(int i = 0; i < cantJugadores; i++)
+    {
+        if(jugadores[i]->getNickname() == nickname){
+            return true;
+        }
+    }
+    return false;
+}
+
+
